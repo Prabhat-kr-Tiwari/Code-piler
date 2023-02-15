cd55b010f22d6f366898652aa485ef99bd5ff072
SHabaj-dev <shabajansari84@gmail.com>
2023-02-15 15:33:19 +0530

    created component package & removed Hello World

--- app/src/main/java/com/prabhat/code_piler/MainActivity.kt
+++ app/src/main/java/com/prabhat/code_piler/MainActivity.kt
@@ -22,22 +22,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Testing Commit and pull")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CodepilerTheme {
-        Greeting("Android")
+
     }
 }
