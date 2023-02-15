12a1f888d2f97d1eacf2e3d209029c11ff7443d1
SHabaj-dev <shabajansari84@gmail.com>
2023-02-15 15:27:51 +0530

    Testing Pull and Push

--- app/src/main/java/com/prabhat/code_piler/MainActivity.kt
+++ app/src/main/java/com/prabhat/code_piler/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Shabaj")
+                    Greeting("Testing Commit and pull")
                 }
             }
         }
