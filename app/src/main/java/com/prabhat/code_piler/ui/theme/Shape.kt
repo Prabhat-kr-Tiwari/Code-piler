fbc6dbceb30365dc3b8694785ce13f7838306b9a
pr847 <Sonukbc6@gmail.com>
2023-02-15 11:02:37 +0530

    Initial commit

---
+++ app/src/main/java/com/prabhat/code_piler/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.prabhat.code_piler.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
