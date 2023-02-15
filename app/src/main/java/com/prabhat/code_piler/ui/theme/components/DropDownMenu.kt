f8756e361b0f9c2fc50beae2858f36b0d5a9cc69
pr847 <Sonukbc6@gmail.com>
2023-02-15 16:00:31 +0530

    Creating DropDown Menu

--- app/src/main/java/com/prabhat/code_piler/ui/theme/components/DropDownMenu.kt
+++ app/src/main/java/com/prabhat/code_piler/ui/theme/components/DropDownMenu.kt
@@ -1,8 +1,121 @@
 package com.prabhat.code_piler.ui.theme.components
 
-import androidx.compose.runtime.Composable
+import android.util.Size
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+/*import androidx.compose.ui.tooling.preview.Preview*/
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+/*import androidx.compose.material.icons.Icons*/
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.geometry.CornerRadius.Companion.Zero
+import androidx.compose.ui.geometry.Offset.Companion.Zero
+/*import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size*/
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 
 @Composable
 fun DropDown() {
 
+}
+// Creating a composable
+// function to display Top Bar
+@Composable
+fun MainContent() {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text("GFG | Drop Down Menu", color = Color.White) }, backgroundColor = Color(0xff0f9d58)) },
+        content = { MyContent() }
+    )
+}
+
+// Creating a composable function
+// to create an Outlined Text Field
+// Calling this function as content
+// in the above function
+@Composable
+fun MyContent(){
+
+    // Declaring a boolean value to store
+    // the expanded state of the Text Field
+    var mExpanded by remember { mutableStateOf(false) }
+
+    // Create a list of cities
+    val mCities = listOf("Delhi", "Mumbai", "Chennai", "Kolkata", "Hyderabad", "Bengaluru", "Pune")
+
+    // Create a string value to store the selected city
+    var mSelectedText by remember { mutableStateOf("") }
+
+//    var mTextFieldSize by remember { mutableStateOf(Size.Zero)}
+
+    // Up Icon when expanded and down icon when collapsed
+    val icon = if (mExpanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+    Column(Modifier.padding(20.dp)) {
+
+        // Create an Outlined Text Field
+        // with icon and not expanded
+        OutlinedTextField(
+            value = mSelectedText,
+            onValueChange = { mSelectedText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    // This value is used to assign to
+                    // the DropDown the same width
+                    mTextFieldSize = coordinates.size.toSize()
+                },
+            label = {Text("Label")},
+            trailingIcon = {
+                Icon(icon,"contentDescription",
+                    Modifier.clickable { mExpanded = !mExpanded })
+            }
+        )
+
+        // Create a drop-down menu with list of cities,
+        // when clicked, set the Text Field text as the city selected
+        DropdownMenu(
+            expanded = mExpanded,
+            onDismissRequest = { mExpanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current){mTextFieldSize.width.toDp()})
+        ) {
+            mCities.forEach { label ->
+                DropdownMenuItem(onClick = {
+                    mSelectedText = label
+                    mExpanded = false
+                }) {
+                    Text(text = label)
+                }
+            }
+        }
+    }
+}
+
+// For displaying preview in
+// the Android Studio IDE emulator
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainContent()
 }
